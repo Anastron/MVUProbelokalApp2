b032de02ca592eb89c5cc215decb8a8b0db26841
Anastron <benz.softwaredeveloping@gmail.com>
2015-09-10 19:02:46 +0200

    initial commit, mit dem schon vorhandenen Code.

---
+++ app/src/main/java/samdev/de/mvuprobelokalapp/SorryMail.java
@@ -0,0 +1,99 @@
+package samdev.de.mvuprobelokalapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class SorryMail extends AppCompatActivity {
+
+    private TextView recipient;
+    private EditText subject;
+    private EditText body;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sorry_mail);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.app_bar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        recipient = (TextView) findViewById(R.id.recipient);
+        subject = (EditText) findViewById(R.id.subject);
+        body = (EditText) findViewById(R.id.body);
+        recipient.setText("entschuldigung@musikverein-unterharmersbach.de");
+
+
+        ImageButton sendBtn = (ImageButton) findViewById(R.id.sendEmail);
+        sendBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                sendEmail();
+                // after sending the email, clear the fields
+                recipient.setText("entschuldigung@musikverein-unterharmersbach.de");
+                subject.setText("");
+                body.setText("");
+            }
+        });
+    }
+
+    protected void sendEmail() {
+
+        String[] recipients = {"benz.armin89@gmail.de"};
+        Intent email = new Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+        // prompts email clients only
+        email.setType("message/rfc822");
+
+        email.putExtra(Intent.EXTRA_EMAIL, recipients);
+        email.putExtra(Intent.EXTRA_SUBJECT, subject.getText().toString());
+        email.putExtra(Intent.EXTRA_TEXT, body.getText().toString());
+
+        try {
+            // the user can choose the email client
+            startActivity(Intent.createChooser(email, "Choose an email client from..."));
+
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(SorryMail.this, "No email client installed.",
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sorry_mail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == android.R.id.home){
+            NavUtils.navigateUpFromSameTask(this);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
