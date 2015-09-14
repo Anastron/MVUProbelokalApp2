7d64e429b5960d9c7becea5a105424cdd7e1a81a
Anastron <benz.softwaredeveloping@gmail.com>
2015-09-14 15:35:25 +0200

    onitemTouchListener für NavigationDrawer hinzugefügt

--- app/src/main/java/samdev/de/mvuprobelokalapp/NavigationDrawerFragment.java
+++ app/src/main/java/samdev/de/mvuprobelokalapp/NavigationDrawerFragment.java
@@ -12,9 +12,12 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -58,6 +61,20 @@ public class NavigationDrawerFragment extends Fragment {
         adapter = new RViewAdapter(getActivity(), getData());
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+
+        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getActivity(), recyclerView, new ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                Toast.makeText(getActivity(), "onClick " + position, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+                Toast.makeText(getActivity(), "onLongClick " + position, Toast.LENGTH_SHORT).show();
+
+            }
+        }));
         return layout;
     }
 
@@ -129,5 +146,62 @@ public class NavigationDrawerFragment extends Fragment {
     public static String readFromPreferences(Context context, String preferenceName, String defaultValue){
         SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_FILE_NAME, Context.MODE_PRIVATE);
        return sharedPreferences.getString(preferenceName,defaultValue);
+    }
+
+    static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener{
+
+        private GestureDetector gestureDetector;
+        private ClickListener clickListener;
+
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final ClickListener clickListener){
+            Log.d("Hmm", "constructor invoked");
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener(){
+
+                @Override
+                public boolean onSingleTapUp(MotionEvent e){
+                    return true;
+                }
+
+                @Override
+                public void onLongPress(MotionEvent e){
+
+                   View child =  recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if(child!=null && clickListener != null)
+                    {
+                        clickListener.onLongClick(child, recyclerView.getChildAdapterPosition(child));
+                    }
+                    super.onLongPress(e);
+                }
+            });
+        }
+
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
+
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+
+            if(child!=null && clickListener!= null && gestureDetector.onTouchEvent(e))
+            {
+                clickListener.onClick(child, rv.getChildAdapterPosition(child));
+            }
+            Log.d("hmm", "onInterceptTouchEvent" + gestureDetector.onTouchEvent(e)+" "+e);
+            return false;
+        }
+
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+            Log.d("Hmm", "onTouchEvent "+e);
+        }
+
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
+
+        }
+    }
+
+    public static interface ClickListener{
+        public void onClick(View view, int position);
+        public void onLongClick(View view, int position);
     }
 }
