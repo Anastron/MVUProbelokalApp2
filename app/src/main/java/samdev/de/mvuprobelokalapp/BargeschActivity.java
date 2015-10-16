1a544bf3f9ad48046ed1fccc0b8a34bd136d8d9a
Anastron <benz.softwaredeveloping@gmail.com>
2015-10-17 00:22:29 +0200

    added tablayout

--- app/src/main/java/samdev/de/mvuprobelokalapp/BargeschActivity.java
+++ app/src/main/java/samdev/de/mvuprobelokalapp/BargeschActivity.java
@@ -1,22 +1,39 @@
 package samdev.de.mvuprobelokalapp;
 
+
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TextView;
+
 
 public class BargeschActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private TabLayout mTabLayout;
+    private ViewPager mPager;
+    private MyPagerAdapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bargesch);
-
+        mAdapter = new MyPagerAdapter(getSupportFragmentManager());
         toolbar = (Toolbar)findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -25,7 +42,17 @@ public class BargeschActivity extends ActionBarActivity {
                 (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mPager = (ViewPager) findViewById(R.id.pager);
+        mPager.setAdapter(mAdapter);
+        mTabLayout.setTabsFromPagerAdapter(mAdapter);
+
+        mTabLayout.setupWithViewPager(mPager);
+        mPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout));
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -61,4 +88,55 @@ public class BargeschActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class MyFragment extends Fragment{
+        public static final java.lang.String ARG_PAGE = "arg_page";
+
+        public MyFragment(){
+
+        }
+
+        public static MyFragment newInstance(int pageNumber){
+            MyFragment myFragment = new MyFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_PAGE, pageNumber);
+            myFragment.setArguments(arguments);
+            return myFragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+            Bundle arguments = getArguments();
+            int pageNumber = arguments.getInt(ARG_PAGE);
+            TextView myText = new TextView(getActivity());
+            myText.setText("Hello I am the text inside this Fragment " + pageNumber);
+            myText.setGravity(Gravity.CENTER);
+            return myText;
+        }
+    }
+
 }
+
+
+class MyPagerAdapter extends FragmentPagerAdapter{
+
+    public MyPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position){
+        BargeschActivity.MyFragment myFragment = BargeschActivity.MyFragment.newInstance(position);
+        return myFragment;
+    }
+
+    @Override
+    public int getCount(){
+        return 3;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        return "Tab " + position;
+    }
+}
+
--- app/src/main/java/samdev/de/mvuprobelokalapp/MainActivity.java
+++ app/src/main/java/samdev/de/mvuprobelokalapp/MainActivity.java
@@ -8,11 +8,12 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.FragmentManager;
+
 
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +26,8 @@ public class MainActivity extends ActionBarActivity {
         NavigationDrawerFragment drawerFragment =
                 (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer,(DrawerLayout)findViewById(R.id.drawer_layout), toolbar);
+
+
 
     }
 
