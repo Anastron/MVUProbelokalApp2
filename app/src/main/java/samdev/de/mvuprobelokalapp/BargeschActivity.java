32e0a914188915b7c6ca31bc97ef1db8e52a11c5
Anastron <benz.softwaredeveloping@gmail.com>
2015-11-02 22:21:35 +0100

    Implementierung Bargeschäfte, alles ist nun möglich

--- app/src/main/java/samdev/de/mvuprobelokalapp/BargeschActivity.java
+++ app/src/main/java/samdev/de/mvuprobelokalapp/BargeschActivity.java
@@ -2,6 +2,8 @@ package samdev.de.mvuprobelokalapp;
 
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.net.Uri;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -38,6 +41,8 @@ public class BargeschActivity extends ActionBarActivity {
     private FloatingActionButton mFAB;
     private LinearLayout mRoot;
 
+    TextView txt_Guthaben;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,16 +58,86 @@ public class BargeschActivity extends ActionBarActivity {
                 (NavigationDrawerFragment)getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
         drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), toolbar);
 
+        final Button btn_BuyBier = (Button) findViewById(R.id.buyBier);
+        final Button btn_BuyGetraenk = (Button) findViewById(R.id.buyGetraenk);
+        final Button btn_BuyMars = (Button) findViewById(R.id.buyMars);
+        final Button btn_BuyDuplo = (Button) findViewById(R.id.buyDuplo);
+        final Button btn_BuyNasch = (Button) findViewById(R.id.buyNaschzeugs);
+        final Button btn_BuySpende = (Button) findViewById(R.id.buyGetraenkSpende);
+
+        txt_Guthaben = (TextView) findViewById(R.id.GuthabenAnzeige);
+
+        aktualisiereGuthaben();
+
+       btn_BuyGetraenk.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                moneyThings(100);
+
+                Snackbar.make(mRoot, "Getränk gekauft", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        btn_BuyMars.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moneyThings(50);
+
+                Snackbar.make(mRoot, "Riegel gekauft", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        btn_BuyDuplo.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moneyThings(30);
+
+                Snackbar.make(mRoot, "Riegel gekauft", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        btn_BuyNasch.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moneyThings(100);
+
+
+                Snackbar.make(mRoot, "Naschzeug gekauft", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        btn_BuySpende.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moneyThings(100);
+
+                Snackbar.make(mRoot, "Du hast etwas spendiert! Sehr gut!", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
+
+        btn_BuyBier.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                moneyThings(100);
+
+                Snackbar.make(mRoot, "Bier gekauft", Snackbar.LENGTH_LONG)
+                        .show();
+            }
+        });
 
         com.getbase.floatingactionbutton.FloatingActionButton actionD = new com.getbase.floatingactionbutton.FloatingActionButton(getBaseContext());
-        //actionC.setTextAlignment();
-        //actionC.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        Snackbar.make(mRoot, "FAB C Clicked", Snackbar.LENGTH_LONG)
-        //                .show();
-        //    }
-        //});
+
 
         final com.getbase.floatingactionbutton.FloatingActionButton actionC = (com.getbase.floatingactionbutton.FloatingActionButton) findViewById(R.id.action_c);
         actionC.setOnClickListener(new View.OnClickListener() {
@@ -70,7 +145,8 @@ public class BargeschActivity extends ActionBarActivity {
             public void onClick(View view) {
                 Snackbar.make(mRoot, "5€ zum Guthaben hinzugefügt", Snackbar.LENGTH_LONG)
                         .show();
-                ;
+
+                moreGuthaben(500);
             }
         });
 
@@ -80,7 +156,8 @@ public class BargeschActivity extends ActionBarActivity {
             public void onClick(View view) {
                 Snackbar.make(mRoot, "10€ zum Guthaben hinzugefügt", Snackbar.LENGTH_LONG)
                         .show();
-                ;
+
+                moreGuthaben(1000);
             }
         });
 
@@ -89,7 +166,9 @@ public class BargeschActivity extends ActionBarActivity {
             @Override
             public void onClick(View view) {
                 Snackbar.make(mRoot, "20€ zum Guthaben hinzugefügt", Snackbar.LENGTH_LONG)
-                        .show();;
+                        .show();
+
+                moreGuthaben(2000);
             }
         });
     }
@@ -99,7 +178,7 @@ public class BargeschActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_bargesch, menu);
         return true;
     }
 
@@ -137,5 +216,49 @@ public class BargeschActivity extends ActionBarActivity {
                     .show();
         }
     };
+
+
+    private void aktualisiereGuthaben(){
+        SharedPreferences guthaben = getSharedPreferences("guthaben", 0);
+        int f_guthaben = guthaben.getInt("guthaben", 0);
+        double dGuthaben = (double) f_guthaben / 100;
+        String txt1 = "Guthaben: " + (dGuthaben) + " €";
+        txt_Guthaben.setText(txt1);
+    }
+
+    private void moreGuthaben(int newGuthaben) {
+
+        SharedPreferences guthaben = getSharedPreferences("guthaben", 0);
+        int f_guthaben = guthaben.getInt("guthaben", 0);
+        f_guthaben += newGuthaben;
+        Editor editor = guthaben.edit();
+        editor.putInt("guthaben", f_guthaben);
+        editor.commit();
+        aktualisiereGuthaben();
+    }
+
+    private void moneyThings(int money)
+    {
+
+        SharedPreferences guthaben = getSharedPreferences("guthaben", 0);
+        int f_guthaben = guthaben.getInt("guthaben", 0);
+        f_guthaben -= money;
+        Editor editor = guthaben.edit();
+        editor.putInt("guthaben", f_guthaben);
+        editor.commit();
+
+        addGeldAus(money);
+        aktualisiereGuthaben();
+    }
+
+    private void addGeldAus(int ausgabe){
+        SharedPreferences statistik = getSharedPreferences("statistik", 0);
+        int menge = statistik.getInt("geldausgegeben", 0);
+        menge += ausgabe;
+        Editor editor = statistik.edit();
+        editor.putInt("geldausgegeben", menge);
+        editor.commit();
+    }
+
 }
 
